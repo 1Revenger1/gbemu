ab196c41175335523af67f27d13a15ad12797a5f
Avery Black <avery.c.black@gmail.com>
2022-07-16 23:04:34 -0700

    Fix timer behavior

--- gbemu/gbtimer.cpp
+++ gbemu/gbtimer.cpp
@@ -2,11 +2,39 @@
 #include "gb.h"
 
 int gbTimer::writeByte(UINT16 addr, UINT8 val) {
+	int overflowVal = (freqToBit[tac & 0x3] / 2) + 1;
+	bool oldEnable = tac & 0x4;
+	bool glitch, newEnable;
+
 	switch (addr) {
-	case 0xFF04: div = 0; break;
-	case 0xFF05: tima = val; break;
-	case 0xFF06: tma = val; break;
-	case 0xFF07: tac = val; break;
+	case 0xFF04:
+		if (oldEnable && div > overflowVal)
+			tima++;
+
+		div = 0;
+		break;
+	case 0xFF05: 
+		if (overflow == Overflow) {
+			overflow = None;
+		}
+
+		if (overflow != Write)
+			tima = val;
+		break;
+	case 0xFF06: 
+		tma = val;
+		if (overflow == Write)
+			tima = val;
+		break;
+	case 0xFF07:
+		newEnable = val & 0x4;
+
+		glitch = !newEnable || (div <= (freqToBit[val & 0x3] / 2) + 1);
+		if (oldEnable && div > overflowVal && glitch)
+			tima++;
+
+		tac = val; 
+		break;
 	default: return -1;
 	}
 	
@@ -24,26 +52,28 @@ int gbTimer::readByte(UINT16 addr) {
 }
 
 void gbTimer::step() {
-	int overflow_bit = freqToBit[tac & 0x3];
-	int overflow_mask = 1 << overflow_bit;
+	int overflowMask = freqToBit[tac & 0x3];
 
 	// Detect falling edge to increment tima
-	int before = div & overflow_mask;
 	div += 4;
-	int after = div & overflow_mask;
 
-	if (overflow) {
-		overflow = false;
-		g_gb->cpu->interruptFlags |= TIMER_INTR;
+	switch (overflow) {
+	case Write: 
+		overflow = None;
+		break;
+	case Overflow:
 		tima = tma;
+		overflow = Write;
+		g_gb->cpu->interruptFlags |= TIMER_INTR;
+		break;
 	}
 
-	if ((tac & 0x4) == 0 || (before - after != overflow_mask)) {
+	if ((tac & 0x4) == 0 || div & overflowMask) {
 		return;
 	}
 
 	tima++;
 	if (tima == 0) {
-		overflow = true;
+		overflow = Overflow;
 	}
 }
--- gbemu/gbtimer.h
+++ gbemu/gbtimer.h
@@ -1,15 +1,22 @@
 #pragma once
-#include <Windows.h>
+#include <BaseTsd.h>
 #include "gbspace.h"
 
 class gbTimer : public gbSpace {
+	enum TIMAOverflow {
+		None,
+		Overflow,
+		Write
+	};
+	
 	UINT16 div = 0;  // Always counts up
 	UINT8 tima = 0;
 	UINT8 tma = 0;  // Initial tima value
 	UINT8 tac = 0;  // Control
 
-	const int freqToBit[4] { 9, 3, 5, 7 };
-	bool overflow = false;
+	const int freqToBit[4] { 1020, 12, 60, 252 };
+	TIMAOverflow overflow = None;
+
 
 public:
 	virtual int writeByte(UINT16 addr, UINT8 val) override;
